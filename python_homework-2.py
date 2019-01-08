3aa6408d1015495ef59e8675c7cb011d1d63de37
Rodney Oliver <roliver3@gmail.com>
2019-01-07 19:21:58 -0500

    homework-2 Functions

---
+++ python_homework-2.py
@@ -0,0 +1,32 @@
+"""
+Python is Easy 
+Functions
+Homework assignment Number 2
+
+"""
+
+def Genre():
+    return "alternative pop-rock"
+
+def Year():
+    return "2016"
+
+def Artist():
+    return "Pan!c at the disco"
+
+def iLoveRapMusic():
+    return False
+
+def iLoveRockMusic():
+    return False
+
+
+print(Genre())
+print(Artist())
+print(Year());
+
+print(iLoveRapMusic())
+print(iLoveRockMusic())
+
+if iLoveRockMusic():
+    print("Fuck yeah!")
